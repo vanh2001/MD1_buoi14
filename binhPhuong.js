0511116ec2bf4fb25122ce15bba56a60b65d8dd1
vanh2001 <anhviet1709@gmail.com>
2022-01-15 15:31:01 +0700

    Add file Binh Phuong

---
+++ binhPhuong.js
@@ -0,0 +1,11 @@
+//khai báo hàm không tham số.
+function checkBinhPhuong() {
+    let x = Number(prompt("Mời bạn nhập vào 1 số: "));
+    let result = x*x;
+    return result;
+}
+//lời gọi hàm:
+
+function getSum(a,b) {
+    return a + b;
+}
